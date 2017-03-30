d8fc7aca90fa887bd5c96ffa5247792a991ceb7c
Sifat Sultan <sifatsultan@gmail.com>
2017-03-30 16:42:19 +0800

    first

---
+++ mapplic/js/process.php
@@ -0,0 +1,25 @@
+<?php
+include("connection.php");
+mysql_select_db("samples",$con);
+$result=mysql_query("select * from student",$con);
+
+echo "<table border='1' >
+<tr>
+<td align=center> <b>Roll No</b></td>
+<td align=center><b>Name</b></td>
+<td align=center><b>Address</b></td>
+<td align=center><b>Stream</b></td></td>
+<td align=center><b>Status</b></td>";
+
+while($data = mysql_fetch_row($result))
+{
+    echo "<tr>";
+    echo "<td align=center>$data[0]</td>";
+    echo "<td align=center>$data[1]</td>";
+    echo "<td align=center>$data[2]</td>";
+    echo "<td align=center>$data[3]</td>";
+    echo "<td align=center>$data[4]</td>";
+    echo "</tr>";
+}
+echo "</table>";
+?>
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Mapplic Plugin Customization
+### 21-Mar
+el means mapplic 'element'
+the thing that we want to change is tooltip, there is one called tooltip-hover as well which we don't really want to edit at the moment.
